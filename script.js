ea955c9f4cb1eda1bbeff5228d1ce681faedbe8c
zidannn24 <zidanmuarif24@gmail.com>
2024-08-30 22:11:23 +0700

    first commit

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-fetchData()
-
-async function fetchData(){
-    const res = await fetch('https://fakestoreapi.com/users')
-    const data = await res.json()
-    console.log(data);
-}
+const bars = document.querySelector('.bars')
+bars.addEventListener('click', function(){
+    const navbarNav = document.querySelector('.navbar-nav')
+    navbarNav.classList.toggle('active')
+})
